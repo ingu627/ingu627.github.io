1386685d5f37d44dc29d9d6c4ce51e39e4d43382
ingu627 <rjsdudans@naver.com>
2022-08-27 23:38:38 +0900

    change theme sunrise -> dirt

--- assets/js/custom/dark-theme.js
+++ assets/js/custom/dark-theme.js
@@ -4,7 +4,7 @@ const darkTheme = [...document.styleSheets].find(style => /(main_dark.css)$/.tes
 let setDarkMode = (isDark) => {
     darkTheme.disabled = isDark !== true;
     defaultTheme.disabled = isDark === true;
-    localStorage.setItem('theme', isDark ? 'dark' : 'sunrise');
+    localStorage.setItem('theme', isDark ? 'dark' : 'dirt');
 }
 
 if (darkTheme) {
