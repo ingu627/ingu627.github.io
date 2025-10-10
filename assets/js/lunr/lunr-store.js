bad5a984791780cf81828a0fc30767745707b2ae
poeun <78655692+ingu627@users.noreply.github.com>
2025-10-10 11:49:46 +0900

    Change method of search ver2

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -5,29 +5,6 @@ layout: none
 var store = [
   {%- for post in site.posts -%}
     {%- if post.search != false -%}
-      {%- assign teaser = nil -%}
-      {%- if post.header.teaser -%}
-        {%- capture teaser -%}{{ post.header.teaser }}{%- endcapture -%}
-      {%- elsif post.header.image -%}
-        {%- capture teaser -%}{{ post.header.image }}{%- endcapture -%}
-      {%- elsif post.image -%}
-        {%- capture teaser -%}{{ post.image }}{%- endcapture -%}
-      {%- else -%}
-        {%- assign content_html = post.content | markdownify -%}
-        {%- assign src_chunks = content_html | split: 'src="' -%}
-        {%- if src_chunks.size > 1 -%}
-          {%- assign teaser_candidate = src_chunks[1] | split: '"' | first -%}
-          {%- capture teaser -%}{{ teaser_candidate }}{%- endcapture -%}
-        {%- endif -%}
-      {%- endif -%}
-      {%- unless teaser -%}
-        {%- capture teaser -%}{{ site.teaser }}{%- endcapture -%}
-      {%- endunless -%}
-      {%- assign teaser = teaser | strip -%}
-      {%- assign teaser_url = teaser -%}
-      {%- unless teaser contains '://' -%}
-        {%- assign teaser_url = teaser | relative_url -%}
-      {%- endunless -%}
       {
         "title": {{ post.title | jsonify }},
         "excerpt":
@@ -54,10 +31,9 @@ var store = [
               replace:"</h6>", " "|
             strip_html | strip_newlines | truncatewords: 50 | jsonify }},
           {%- endif -%}
-        "categories": {{ post.categories | jsonify }},
-  "tags": {{ post.tags | jsonify }},
-  "url": {{ post.url | relative_url | jsonify }},
-  "teaser": {{ teaser_url | jsonify }}
+    "categories": {{ post.categories | jsonify }},
+    "tags": {{ post.tags | jsonify }},
+    "url": {{ post.url | relative_url | jsonify }}
       }{%- unless forloop.last -%},{%- endunless -%}
     {%- endif -%}
   {%- endfor -%}
@@ -67,29 +43,6 @@ var store = [
     {%- endif -%}
     {%- assign docs = c.docs | where_exp:'doc','doc.search != false' -%}
     {%- for doc in docs -%}
-      {%- assign teaser = nil -%}
-      {%- if doc.header.teaser -%}
-        {%- capture teaser -%}{{ doc.header.teaser }}{%- endcapture -%}
-      {%- elsif doc.header.image -%}
-        {%- capture teaser -%}{{ doc.header.image }}{%- endcapture -%}
-      {%- elsif doc.image -%}
-        {%- capture teaser -%}{{ doc.image }}{%- endcapture -%}
-      {%- else -%}
-        {%- assign content_html = doc.content | markdownify -%}
-        {%- assign src_chunks = content_html | split: 'src="' -%}
-        {%- if src_chunks.size > 1 -%}
-          {%- assign teaser_candidate = src_chunks[1] | split: '"' | first -%}
-          {%- capture teaser -%}{{ teaser_candidate }}{%- endcapture -%}
-        {%- endif -%}
-      {%- endif -%}
-      {%- unless teaser -%}
-        {%- capture teaser -%}{{ site.teaser }}{%- endcapture -%}
-      {%- endunless -%}
-      {%- assign teaser = teaser | strip -%}
-      {%- assign teaser_url = teaser -%}
-      {%- unless teaser contains '://' -%}
-        {%- assign teaser_url = teaser | relative_url -%}
-      {%- endunless -%}
       ,{
         "title": {{ doc.title | jsonify }},
         "excerpt":
@@ -116,10 +69,9 @@ var store = [
               replace:"</h6>", " "|
             strip_html | strip_newlines | truncatewords: 50 | jsonify }},
           {%- endif -%}
-        "categories": {{ doc.categories | jsonify }},
+  "categories": {{ doc.categories | jsonify }},
   "tags": {{ doc.tags | jsonify }},
-  "url": {{ doc.url | relative_url | jsonify }},
-  "teaser": {{ teaser_url | jsonify }}
+  "url": {{ doc.url | relative_url | jsonify }}
       }{%- unless forloop.last and l -%},{%- endunless -%}
     {%- endfor -%}
   {%- endfor -%}]
