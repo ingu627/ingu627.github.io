3ca97e3582212d9a141f0214bbd3b6d284342da4
ingu627 <rjsdudans@naver.com>
2025-10-13 22:25:08 +0900

    Optimize ads layout and add service worker caching

--- _pages/search.md
+++ _pages/search.md
@@ -4,5 +4,4 @@ layout: search
 permalink: /search/
 ---
 
-<br>
-<br>
+아래 입력창에 검색어를 입력하면 관련 글이 표시됩니다.
--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,76 +2,46 @@
 layout: none
 ---
 
+{%- assign docs = site.posts | where_exp:'post','post.search != false' -%}
+{%- for coll in site.collections -%}
+  {%- unless coll.label == 'posts' -%}
+    {%- assign filtered = coll.docs | where_exp:'doc','doc.search != false' -%}
+    {%- assign docs = docs | concat: filtered -%}
+  {%- endunless -%}
+{%- endfor -%}
+{%- assign docs = docs | group_by: 'url' -%}
 var store = [
-  {%- for post in site.posts -%}
-    {%- if post.search != false -%}
-      {
-        "title": {{ post.title | jsonify }},
-        "excerpt":
-          {%- if site.search_full_content == true -%}
-            {{ post.content | newline_to_br |
-              replace:"<br />", " " |
-              replace:"</p>", " " |
-              replace:"</h1>", " " |
-              replace:"</h2>", " " |
-              replace:"</h3>", " " |
-              replace:"</h4>", " " |
-              replace:"</h5>", " " |
-              replace:"</h6>", " "|
-            strip_html | strip_newlines | jsonify }},
-          {%- else -%}
-            {{ post.content | newline_to_br |
-              replace:"<br />", " " |
-              replace:"</p>", " " |
-              replace:"</h1>", " " |
-              replace:"</h2>", " " |
-              replace:"</h3>", " " |
-              replace:"</h4>", " " |
-              replace:"</h5>", " " |
-              replace:"</h6>", " "|
-            strip_html | strip_newlines | truncatewords: 50 | jsonify }},
-          {%- endif -%}
-    "categories": {{ post.categories | jsonify }},
-    "tags": {{ post.tags | jsonify }},
-    "url": {{ post.url | relative_url | jsonify }}
-      }{%- unless forloop.last -%},{%- endunless -%}
-    {%- endif -%}
+  {%- for group in docs -%}
+    {%- assign doc = group.items[0] -%}
+    {
+      "title": {{ doc.title | jsonify }},
+      "excerpt":
+        {%- if site.search_full_content == true -%}
+          {{ doc.content | newline_to_br |
+            replace:"<br />", " " |
+            replace:"</p>", " " |
+            replace:"</h1>", " " |
+            replace:"</h2>", " " |
+            replace:"</h3>", " " |
+            replace:"</h4>", " " |
+            replace:"</h5>", " " |
+            replace:"</h6>", " "|
+          strip_html | strip_newlines | jsonify }},
+        {%- else -%}
+          {{ doc.content | newline_to_br |
+            replace:"<br />", " " |
+            replace:"</p>", " " |
+            replace:"</h1>", " " |
+            replace:"</h2>", " " |
+            replace:"</h3>", " " |
+            replace:"</h4>", " " |
+            replace:"</h5>", " " |
+            replace:"</h6>", " "|
+          strip_html | strip_newlines | truncatewords: 30 | jsonify }},
+        {%- endif -%}
+      "categories": {{ doc.categories | jsonify }},
+      "tags": {{ doc.tags | jsonify }},
+      "url": {{ doc.url | relative_url | jsonify }}
+    }{%- unless forloop.last -%},{%- endunless -%}
   {%- endfor -%}
-  {%- for c in site.collections -%}
-    {%- if forloop.last -%}
-      {%- assign l = true -%}
-    {%- endif -%}
-    {%- assign docs = c.docs | where_exp:'doc','doc.search != false' -%}
-    {%- for doc in docs -%}
-      ,{
-        "title": {{ doc.title | jsonify }},
-        "excerpt":
-          {%- if site.search_full_content == true -%}
-            {{ doc.content | newline_to_br |
-              replace:"<br />", " " |
-              replace:"</p>", " " |
-              replace:"</h1>", " " |
-              replace:"</h2>", " " |
-              replace:"</h3>", " " |
-              replace:"</h4>", " " |
-              replace:"</h5>", " " |
-              replace:"</h6>", " "|
-            strip_html | strip_newlines | jsonify }},
-          {%- else -%}
-            {{ doc.content | newline_to_br |
-              replace:"<br />", " " |
-              replace:"</p>", " " |
-              replace:"</h1>", " " |
-              replace:"</h2>", " " |
-              replace:"</h3>", " " |
-              replace:"</h4>", " " |
-              replace:"</h5>", " " |
-              replace:"</h6>", " "|
-            strip_html | strip_newlines | truncatewords: 50 | jsonify }},
-          {%- endif -%}
-  "categories": {{ doc.categories | jsonify }},
-  "tags": {{ doc.tags | jsonify }},
-  "url": {{ doc.url | relative_url | jsonify }}
-      }{%- unless forloop.last and l -%},{%- endunless -%}
-    {%- endfor -%}
-  {%- endfor -%}]
+]
